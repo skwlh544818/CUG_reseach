323e3de2341a6987db8ca4a6b7cb20deab1b3229
skwlh544818 <69035874+skwlh544818@users.noreply.github.com>
2020-12-14 18:52:01 +0800

    oj1229

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
+int max(int a,int b)
+{
+    if (a>b)
+        return a;
+    else
+        return b;
+}
 int main()
 {
-    int x,y,z;
-    scanf("%d %d %d",&x,&y,&z);
-    printf("%d\n",x+y+z);
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%d\n",max(max(a,b),c));
     return 0;
 }

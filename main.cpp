f593547537dfde1928aafe07901fd6943040d346
skwlh544818 <69035874+skwlh544818@users.noreply.github.com>
2020-12-14 18:44:21 +0800

    oj1224

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int a;
-    scanf("%d",&a);
-    printf("%d\n",a);
+    int x,y;
+    scanf("%d %d",&x,&y);
+    int p=x*y;
+    int s=x+y;
+    int total=s*s+p*(s-x)*(p+y);
+    printf("%d\n",total);
     return 0;
 }

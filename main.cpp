ffb46261b845b39f3402dbac0d6232b6ce6ea067
skwlh544818 <69035874+skwlh544818@users.noreply.github.com>
2020-12-14 20:01:29 +0800

    oj1417

--- main.cpp
+++ main.cpp
@@ -1,26 +1,44 @@
 #include <stdio.h>
+
+int checkChopsticks(int *chopsticks,int n)
+{
+    if(chopsticks == NULL)
+    {
+        return -1;
+    }
+    int recod[20]={0};
+    int count = 0, index = 0;
+
+    /*foreach the chopsticks*/
+    for(int i = 0; i < n; i++)
+    {
+        recod[chopsticks[i]]++;
+    }
+
+    for(int i = 0; i < recod.length; i++)
+    {
+        if(recod[i] % 2 == 1)
+        {
+            count++;
+            index = i;
+        }
+    }
+
+    if(count == 0 || count > 1)
+    {
+        return -1;
+    }
+    return index;
+}
+
 int main()
 {
-    int n,a,b[1000]={0};
+    int n,a[20];
     scanf("%d",&n);
-    if(n%2==0)
+    for(int i=0;i<n;++i)
     {
-        printf("-1");
+        scanf("%d",a+i);
     }
-    else{
-        for(int i=0;i<n;++i)
-        {
-            scanf("%d",&a);
-            b[a]++;
-        }
-        for(int i=0;i<1000;++i)
-        {
-            if (b[i]%2==1)
-            {
-                printf("%d\n",i);
-                break;
-            }
-        }
-    }
+    printf("%d\n",ChopSticks(a,n));
     return 0;
 }

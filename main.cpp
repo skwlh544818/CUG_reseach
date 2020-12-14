2dd55f50bf66e4ee43810c71c34081797710f8f5
skwlh544818 <69035874+skwlh544818@users.noreply.github.com>
2020-12-14 18:58:35 +0800

    oj1230

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <stdio.h>
-int max(int a,int b)
-{
-    if (a>b)
-        return a;
-    else
-        return b;
-}
 int main()
 {
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    printf("%d\n",max(max(a,b),c));
+    double a,b,c;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    double avg=(a+b+c)/3;
+    if (avg>=90)printf("A\n");
+    else if(avg>=70 &&avg<90) printf("B\n");
+    else if (avg>=50 && avg<70)printf("C\n");
+    else if (avg<50)printf("F\n");
     return 0;
 }
